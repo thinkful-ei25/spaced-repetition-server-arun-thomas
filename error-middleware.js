877c692ff8fc356b0fb262b5b84695ef7beb3b08
Arun Seehra <seehra.a@gmail.com>
2019-01-07 13:11:44 -0600

    Use error.code rather than error.status
    The client side uses `code` consistently, so we should match that usage.

--- error-middleware.js
+++ error-middleware.js
@@ -3,19 +3,19 @@
 // Custom 404 Not Found route handler
 const error404 = (req, res, next) => {
   const err = new Error('Not Found');
-  err.status = 404;
+  err.code = 404;
   next(err);
 };
 
 // Custom Error Handler
 // eslint-disable-next-line no-unused-vars
 const error500 = (err, req, res, next) => {
-  if (err.status) {
+  if (err.code) {
     const errBody = Object.assign({}, err, { message: err.message });
-    res.status(err.status).json(errBody);
+    res.status(err.code).json(errBody);
   } else {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: 'Internal Server Error', code: 500 });
   }
 };
 
