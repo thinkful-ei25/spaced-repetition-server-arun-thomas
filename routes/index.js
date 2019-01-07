425a8ffca2fe38e932abbe56c752e8826c29f722
Arun Seehra <seehra.a@gmail.com>
2019-01-07 16:06:35 -0600

    Hardcode question objects for front end testing

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const express = require('express');
 
 const users = require('./users');
 const auth = require('./auth');
+const question = require('./question');
 
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/auth', auth);
+router.use('/question', question);
 
 module.exports = router;
