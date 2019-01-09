d598fc9f4e62393aa421b9a16c19757a459d2d24
ThomasChun <tchun2055@gmail.com>
2019-01-09 08:47:42 -1000

    added history route

---
+++ routes/history.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+router.use(jwtAuth);
+
+router.get('/', (req, res, next) => {
+  User.findById(req.user.id)
+    .then((user) => {
+      const returnedData = user.questionData.map(data => {
+        const { question, history } = data;
+        return {
+          id: question.id,
+          text: question.text,
+          history,
+        }
+      })
+      res.json({ questions: returnedData});
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const express = require('express');
 const users = require('./users');
 const auth = require('./auth');
 const question = require('./question');
+const history = require('./history');
 
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/auth', auth);
 router.use('/question', question);
+router.use('/history', history);
 
 module.exports = router;
